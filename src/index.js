9dd7ed4415b836087a14f56288577e0436b6cc68
Mahima382 <sultanamahima384@gmail.com>
2025-11-04 19:51:10 +0600

    Assisgnment Task-1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 const tasks = [
        { id: 1, title: 'Learn Node.js', completed: false },
-       { id: 2, title: 'Build REST API', completed: false }
+       { id: 2, title: 'Build REST API', completed: false },
+       { id: 3, title: "Learn MySQL", completed: false },
+       { id: 4, title: "Setup Postman", completed: true },
+       { id: 5, title: "Practice Git", completed: false }
 ];
 app.get('/', (req, res) => {
        res.send('Task Management API is running!');
